c0b2d0a17191104419a39cf60a6e6103bd9a86fe
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-06 14:30:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-      @@all.find{|all_names| Song.name == name}
+      @@all.find{|all_names| all_names == name}
       binding.pry
     end
 end
