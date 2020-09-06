e0b68c4e014044e68f7db8f88e4f668bc0a4eb6d
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-06 15:09:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
      song 
    end
    
-   def self.create_from_filename 
+   def self.create_from_filename(filename) 
      split_filename = filename.slice(0, filename.length-4).split(" - ")
      song = Song.new 
      song.name = split_filename[1] 
